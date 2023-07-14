ff5ab875b0e098a7b35f0fe85e2a88df02482bf1
Jefferson <jestrada@celerix.com>
2023-07-14 16:14:33 -0500

    Revert "Revert "prueba03-cris""
    This reverts commit 862111a7fda6422cd819a8e05e287fb2e5ce1ff3.
    
    Reverso 2.

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import time
 
 def main():
     print('Hola mundo')
-    time.sleep(15)
+    time.sleep(10)
         
 if __name__ == "__main__":
     app = Flask(__name__)
     @app.route('/')
     def index():
         main()  
-        return 'Procesamiento completado version 2'  
+        return 'Procesamiento completado version 3'  
 
     app.run(host='0.0.0.0', port=8080)
