006870bb67fcdbb7faff5ab22bdbc79db6ba3bd9
Jefferson <jestrada@celerix.com>
2023-07-14 15:05:03 -0500

    prueba02

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
 from flask import Flask
-
+import time
 
 
 def main():
     print('Hola mundo')
+    time.sleep(15)
         
-
 if __name__ == "__main__":
     app = Flask(__name__)
     @app.route('/')
     def index():
         main()  
-        return 'Procesamiento completado'  
+        return 'Procesamiento completado version 2'  
 
     app.run(host='0.0.0.0', port=8080)
